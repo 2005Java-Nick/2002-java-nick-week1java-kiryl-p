fabcd1371408aa711d20fe5c4daef5996aa4b6a3
kiryl_p <kiryl.pashkouski@gmail.com>
2020-05-08 08:56:52 -0400

    added #16

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -505,6 +505,15 @@ public class EvaluationService {
 	 */
 	public boolean isPangram(String string) {
 		// TODO Write an implementation for this method declaration
+		String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+				"k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+		int counter = 0;
+		for (int i = 0; i < alphabet.length; i++) {
+			if (string.contains(alphabet[i])) {
+				counter++;
+			}
+		}
+		if (counter == 26) return true;
 		return false;
 	}
 
